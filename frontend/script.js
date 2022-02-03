1e8653f611c3ea79860286708c494dc8f8a0f878
toth_szilvia <szylwy@gmail.com>
2022-02-03 17:55:49 +0100

    add header and links from js

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,31 @@
-function addTwoIntegers(firstInteger, secondInteger){
-    // console.log(firstInteger + secondInteger);
-    // return firstInteger + secondInteger;
-    // return "hello";
-    if(typeof(firstInteger) === "number" && typeof(secondInteger) === "number"){
-        return firstInteger + secondInteger
-    } else{
-        return "One of the arguments is not a number, and the functuin is not applicable."
+function loadEvent(eventObject){
+    // console.log(eventObject);
+    // const //dont want to change its value
+    // let // wanna change the value
+    const rootElement = document.getElementById("root");
+    // console.dir(document);
+    // console.dir(rootElement); //this way in every browser it will be shown on the Console log the log() isnot
+
+    const listOfSectionElements = document.querySelectorAll("section"); //the parameter shuold be like a css selector
+    console.log(listOfSectionElements);
+
+    rootElement.classList.add("newClass");
+
+    let anchors = ""; //
+    for (const sectionElement of listOfSectionElements) {
+        // sectionElement.classList.add("newClass");
+        // anchors = anchors + `<a> ${ sectionElement.id } </a>`; // `` this is the multiple line string, ${} template literal
+        anchors += `<a href="#${ sectionElement.id }"> ${ sectionElement.id } </a>`;
     }
+    console.log(anchors);
+    rootElement.insertAdjacentHTML("afterbegin", `<header>${anchors}</header>`); // afterbegin a legelső elemek elé kerül
+
+    // listOfSectionElements[0].classList.add("newClass");
+    // listOfSectionElements[1].classList.add("newClass");
+    // listOfSectionElements[2].classList.add("newClass");
+    // listOfSectionElements[3].classList.add("newClass");
+
+    console.dir(rootElement);
 }
 
-console.log(addTwoIntegers(12, "13") );
-console.log(addTwoIntegers(1920, 85) );
-console.log(0==="0");
+window.addEventListener("load", loadEvent);
